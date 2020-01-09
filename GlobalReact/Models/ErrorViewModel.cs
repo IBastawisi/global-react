f504ff82fdbd68908d80ccf820c44bd2bb81d69a
IBastawisi <IBastawisi@outlook.com>
2020-01-09 14:00:15 +0200

    Add project files.

---
+++ GlobalReact/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GlobalReact.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
